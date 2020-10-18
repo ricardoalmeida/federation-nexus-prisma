00159e5946513b0e054ea68cec6263e3766849f0
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2020-10-18 18:47:22 +0200

    Add fallback for userId

--- src/context.ts
+++ src/context.ts
@@ -1,15 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient({ log: ['query'] });
+
 export interface Context {
   prisma: PrismaClient;
   userId: string;
   permissions: Set<string>;
 }
 
-const prismaClient = new PrismaClient({ log: ['query'] });
 export const createContext = ({ req: request }: { req: any }): Context => {
-  const userId = request?.headers['user-uuid'];
+  // TODO: remove fallback when gateway sends the header
+  const userId = request?.headers['user-uuid'] || '123456';
   const permissions = new Set<string>(request?.headers.permissions);
 
-  return { prisma: prismaClient, userId, permissions };
+  return { prisma, userId, permissions };
 };
