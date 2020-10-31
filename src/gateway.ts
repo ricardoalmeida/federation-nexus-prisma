6dbc8f37b570da342f9d78d7a2f3222a7f42c452
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2020-10-31 20:15:50 +0100

    Add gateway to the project

---
+++ src/gateway.ts
@@ -0,0 +1,44 @@
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+const { ApolloGateway, RemoteGraphQLDataSource } = require('@apollo/gateway');
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  constructor(opts: any) {
+    super(opts);
+  }
+  willSendRequest({ request, context }: { request: any; context: any }) {
+    request.http.headers.set('user-id', context.userID);
+  }
+}
+
+const gateway = new ApolloGateway({
+  serviceList: [{ name: 'share-music', url: 'http://localhost:4001/graphql' }],
+  buildService({ url }: { url: string }) {
+    return new AuthenticatedDataSource({ url });
+  },
+});
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+  context: ({ req }: any) => {
+    const token = req.headers.authorization || '';
+
+    // parse JWT into scope and user identity
+    // const userID = getUserId(token);
+    const userID = '1';
+
+    return { userID };
+  },
+});
+
+const port = 4000;
+async function run() {
+  const app = express();
+  server.applyMiddleware({ app });
+  app.listen({ port }, () => {
+    console.log(`🚀 Server ${process.env.NODE_ENV} ready at http://localhost:${port}/graphql`);
+  });
+}
+
+run();
