d8bf5a89bd14ad47ac713afbbbf9648a83669cc4
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2020-11-04 23:57:02 +0100

    Use packages folder

--- packages/vitrola/src/schema.ts
+++ packages/vitrola/src/schema.ts
@@ -60,6 +60,7 @@ const schema = makeSchema({
   plugins: [nexusPrisma({ experimentalCRUD: true })],
   outputs: {
     schema: path.join(process.cwd(), './generated/schema.graphql'),
+    typegen: path.join(__dirname, '../../node_modules/@types/nexus-typegen/index.d.ts'),
   },
   typegenAutoConfig: {
     contextType: 'Context.Context',
