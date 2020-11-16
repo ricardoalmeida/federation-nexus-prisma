5ebd69b6a7ee566b739fb6859bd9362252c161ab
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2020-11-16 22:13:40 +0100

    Organize folders

---
+++ src/resolvers/Mutations.ts
@@ -0,0 +1,26 @@
+import { mutationType } from '@nexus/schema';
+import { Context } from '../context';
+
+export const Mutation = mutationType({
+  definition(t) {
+    t.crud.createOnePlaylist({
+      alias: 'createPlaylist',
+      async resolve(root: any, args: any, ctx: Context, info: any, originalResolve: any) {
+        args = {
+          ...args,
+          data: {
+            ...args.data,
+            userId: ctx.userId,
+          },
+        };
+        const res = await originalResolve(root, args, ctx, info);
+        return res;
+      },
+    });
+    t.crud.updateOnePlaylist();
+    t.crud.deleteOnePlaylist({ alias: 'deletePlaylist' });
+    t.crud.createOneTrack();
+    t.crud.updateOneTrack();
+    t.crud.deleteOneTrack({ alias: 'deleteTrack' });
+  },
+});
--- src/schema.ts
+++ src/schema.ts
@@ -1,62 +1,13 @@
-import { makeSchema, mutationType, objectType, queryType } from '@nexus/schema';
+import { makeSchema } from '@nexus/schema';
 import { applyMiddleware } from 'graphql-middleware';
 import { transformSchemaFederation } from 'graphql-transform-federation';
 import { nexusPrisma } from 'nexus-plugin-prisma';
 import path from 'path';
-import { Context } from './context';
+import * as allTypes from './resolvers';
 import { permissions } from './utils/permissions';
 
-export const Playlist = objectType({
-  name: 'Playlist',
-  definition(t) {
-    t.model.id();
-    t.model.description();
-    t.model.userId();
-    t.model.tracks({ type: 'Track' });
-  },
-});
-
-export const Track = objectType({
-  name: 'Track',
-  definition(t) {
-    t.model.id();
-    t.model.name();
-  },
-});
-
-const Query = queryType({
-  definition(t) {
-    t.crud.playlist();
-    t.crud.playlists({ pagination: true, filtering: true });
-  },
-});
-
-const Mutation = mutationType({
-  definition(t) {
-    t.crud.createOnePlaylist({
-      alias: 'createPlaylist',
-      async resolve(root: any, args: any, ctx: Context, info: any, originalResolve: any) {
-        args = {
-          ...args,
-          data: {
-            ...args.data,
-            userId: ctx.userId,
-          },
-        };
-        const res = await originalResolve(root, args, ctx, info);
-        return res;
-      },
-    });
-    t.crud.updateOnePlaylist();
-    t.crud.deleteOnePlaylist({ alias: 'deletePlaylist' });
-    t.crud.createOneTrack();
-    t.crud.updateOneTrack();
-    t.crud.deleteOneTrack({ alias: 'deleteTrack' });
-  },
-});
-
 const schema = makeSchema({
-  types: [Query, Mutation, Playlist, Track],
+  types: [allTypes],
   plugins: [nexusPrisma({ experimentalCRUD: true })],
   outputs: {
     schema: path.join(process.cwd(), './generated/schema.graphql'),
